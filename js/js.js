477344ce61196411c1dab5c75e099179552070ac
Caroline Paier <carolinelopespaier@gmail.com>
2022-03-12 13:26:49 -0300

    master

--- js/js.js
+++ js/js.js
@@ -7,6 +7,7 @@ function start() { // Inicio da fun��o start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class= 'anima3'></div>");
 	$("#fundoGame").append("<div id='placar'></div>");
+	$("#fundoGame").append("<div id='energia'></div>");
 
 
 //Principais variáveis do jogo
@@ -17,6 +18,7 @@ var fimdejogo=false;
 var pontos=0;
 var salvos=0;
 var perdidos=0;
+var energiaAtual=3;
 var jogo = {};
 var velocidade=5;
 var posicaoY = parseInt(Math.random() * 334);
@@ -27,7 +29,18 @@ var TECLA = {
 	}
 
 	jogo.pressionou = [];
+
+	var somDisparo=document.getElementById("somDisparo");
+	var somExplosao=document.getElementById("somExplosao");
+	var musica=document.getElementById("musica");
+	var somGameover=document.getElementById("somGameover");
+	var somPerdido=document.getElementById("somPerdido");
+	var somResgate=document.getElementById("somResgate");
 	
+//Música em loop
+musica.addEventListener("ended", function(){ musica.currentTime = 0; musica.play(); }, false);
+musica.play();
+
 //Verifica se o usuário pressionou alguma tecla	
 	
 $(document).keydown(function(e){
@@ -52,6 +65,7 @@ moveinimigo2();
 moveamigo();
 colisao();
 placar();
+energia();
 
 } // Fim da função loop()
 
@@ -139,7 +153,8 @@ function movefundo() {
 	function disparo() {
 	
 		if (podeAtirar==true) {
-			
+		
+		somDisparo.play();	
 		podeAtirar=false;
 		
 		topo = parseInt($("#jogador").css("top"))
@@ -182,6 +197,7 @@ var colisao6 = ($("#inimigo2").collision($("#amigo")));
 	
 	if (colisao1.length>0) {
 		
+	energiaAtual--;
 	inimigo1X = parseInt($("#inimigo1").css("left"));
 	inimigo1Y = parseInt($("#inimigo1").css("top"));
 	explosao1(inimigo1X,inimigo1Y);
@@ -194,6 +210,7 @@ var colisao6 = ($("#inimigo2").collision($("#amigo")));
 // jogador com o inimigo2 
 if (colisao2.length>0) {
 	
+	energiaAtual--;
 	inimigo2X = parseInt($("#inimigo2").css("left"));
 	inimigo2Y = parseInt($("#inimigo2").css("top"));
 	explosao2(inimigo2X,inimigo2Y);
@@ -209,6 +226,7 @@ if (colisao2.length>0) {
 		
 if (colisao3.length>0) {
 
+	velocidade=velocidade+0.3;
 	pontos=pontos+100;			
 	inimigo1X = parseInt($("#inimigo1").css("left"));
 	inimigo1Y = parseInt($("#inimigo1").css("top"));
@@ -243,6 +261,7 @@ if (colisao3.length>0) {
 if (colisao5.length>0) {
 
 	salvos++;		
+	somResgate.play();
 	reposicionaAmigo();
 	$("#amigo").remove();
 	}
@@ -265,7 +284,8 @@ if (colisao6.length>0) {
 
 	//Explosão 1
 
-function explosao1(inimigo1X,inimigo1Y) {
+    function explosao1(inimigo1X,inimigo1Y) {
+	somExplosao.play();
 	$("#fundoGame").append("<div id='explosao1'></div");
 	$("#explosao1").css("background-image", "url(imgs/explosao.png)");
 	var div=$("#explosao1");
@@ -327,7 +347,8 @@ function reposicionaInimigo2() {
 //Explosão2
 	
 function explosao2(inimigo2X,inimigo2Y) {
-	
+
+	somExplosao.play();
 	$("#fundoGame").append("<div id='explosao2'></div");
 	$("#explosao2").css("background-image", "url(imgs/explosao.png)");
 	var div2=$("#explosao2");
@@ -351,6 +372,8 @@ function explosao2(inimigo2X,inimigo2Y) {
 //Explosão3
 	
 function explosao3(amigoX,amigoY) {
+	
+	somPerdido.play();
 	$("#fundoGame").append("<div id='explosao3' class='anima4'></div");
 	$("#explosao3").css("top",amigoY);
 	$("#explosao3").css("left",amigoX);
@@ -369,5 +392,34 @@ function explosao3(amigoX,amigoY) {
 		$("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
 		
 	} //fim da função placar()
+
+//Barra de energia
+
+function energia() {
+	
+	if (energiaAtual==3) {
+		
+		$("#energia").css("background-image", "url(imgs/energia3.png)");
+	}
+
+	if (energiaAtual==2) {
+		
+		$("#energia").css("background-image", "url(imgs/energia2.png)");
+	}
+
+	if (energiaAtual==1) {
+		
+		$("#energia").css("background-image", "url(imgs/energia1.png)");
+	}
+
+	if (energiaAtual==0) {
+		
+		$("#energia").css("background-image", "url(imgs/energia0.png)");
+		
+		//Game Over
+	}
+
+} // Fim da função energia()
+
 	
 } // Fim da função start
